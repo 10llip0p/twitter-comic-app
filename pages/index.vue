1cc0340c873d9939b7aff13c626ef396edd577c6
YutoKoguchi <kc.u10llip0p@gmail.com>
2019-04-14 00:39:03 +0900

    Update: form ui with vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,44 @@
 <template>
-  <section class="container">
-    <div>
-      <form
-        style="display: flex; justify-content: center"
-        @submit.prevent="fetchTweets"
-      >
-        <vs-input
-          v-model="tweetUrl"
-          label-placeholder="最後のページのURL"
-          style="display: inline"
-        />
-        <vs-button color="success" type="filled submit">見る</vs-button>
-      </form>
-    </div>
-  </section>
+  <v-app>
+    <v-toolbar color="cyan" dark fixed app>
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-form ref="form" v-model="isValid" lazy-validation>
+          <v-text-field
+            v-model="tweetUrl"
+            label="最後のページのURL"
+            single-line
+            outline
+            append-icon="search"
+            required
+            :rules="[
+              v =>
+                /^https?:\/\/twitter.com\/\w+\/status\/\d+$/.test(v) ||
+                'URLが不正'
+            ]"
+            @click:append="fetchTweets"
+          ></v-text-field>
+        </v-form>
+        <!--
+        <form
+          style="display: flex; justify-content: center"
+          @submit.prevent="fetchTweets"
+        >
+          <vs-input
+            v-model="tweetUrl"
+            label-placeholder="最後のページのURL"
+            style="display: inline"
+          />
+          <vs-button color="success" type="filled submit">見る</vs-button>
+        </form>
+-->
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -21,21 +46,23 @@ export default {
   data() {
     return {
       tweetUrl: '',
-      isInValid: true
+      isValid: true
     }
   },
   methods: {
     fetchTweets() {
-      const tweetId = this.tweetUrl.match(
-        /^https?:\/\/twitter.com\/\w+\/status\/(\d+)$/
-      )
-      if (!tweetId) {
-        // TODO: バリデーション
-        return
+      if (this.$refs.form.validate()) {
+        const tweetId = this.tweetUrl.match(
+          /^https?:\/\/twitter.com\/\w+\/status\/(\d+)$/
+        )
+        if (!tweetId) {
+          // TODO: バリデーション
+          return
+        }
+        this.$router.push({
+          path: `/tweet/${tweetId[1]}`
+        })
       }
-      this.$router.push({
-        path: `/tweet/${tweetId[1]}`
-      })
     }
   }
 }
