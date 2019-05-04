e771c9b68edfe4196b660eae832a54d3b13ba66f
YutoKoguchi <kc.u10llip0p@gmail.com>
2019-05-04 23:30:06 +0900

    Add oauth signin and callback

---
+++ pages/signin/callback.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>hoge</div>
+</template>
+
+<script>
+export default {
+  asyncData({ query }) {
+    return {
+      oauthToken: query.oauth_token,
+      oauthVerifier: query.oauth_verifier
+    }
+  },
+  mounted() {
+    // TODO アクセストークン取得処理
+    console.log(this.oauthToken)
+    console.log(this.oauthVerifier)
+  }
+}
+</script>
---
+++ pages/signin/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>hoge</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData() {
+    const oauthParams = await axios.get(`${process.env.baseUrl}/api/signin`)
+    return {
+      oauthUrl: oauthParams.data.oauth_url,
+      oauthTokenSecret: oauthParams.data.oauth_token_secret
+    }
+  },
+  mounted() {
+    window.location.href = this.oauthUrl
+  }
+}
+</script>
